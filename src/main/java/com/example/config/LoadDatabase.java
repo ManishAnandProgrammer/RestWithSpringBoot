d517b0b46a0f623e3654867bd229c796742e76b4
Manish Anand <manishanandprogrammer@gmail.com>
2021-04-26 07:39:01 +0530

    Order Example with links complete

--- src/main/java/com/example/config/LoadDatabase.java
+++ src/main/java/com/example/config/LoadDatabase.java
@@ -1,7 +1,10 @@
 package com.example.config;
 
+import com.example.enums.Status;
 import com.example.modal.Employee;
+import com.example.modal.Order;
 import com.example.repository.EmployeeRepository;
+import com.example.repository.OrderRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -14,12 +17,22 @@ class LoadDatabase {
   private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
   @Bean
-  CommandLineRunner initDatabase(EmployeeRepository repository) {
+  CommandLineRunner initDatabase(EmployeeRepository employeeRepository, OrderRepository orderRepository) {
 
     return args -> {
-      log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar")));
+      employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar"));
+      employeeRepository.save(new Employee("Frodo", "Baggins", "thief"));
 
-      log.info("Preloading " + repository.save(new Employee("Frodo", "Baggins", "thief")));
+      employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
+
+
+      orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+      orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+
+      orderRepository.findAll().forEach(order -> {
+        log.info("Preloaded " + order);
+      });
+
     };
   }
 }
